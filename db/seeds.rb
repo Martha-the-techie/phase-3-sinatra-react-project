c03cf568dabfcd2edb24c7cdc1783dcc2f3508aa
Mumbi30! <github email address>
2023-06-09 21:29:53 +0300

    Completed assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,31 @@
 puts "🌱 Seeding cakes..."
 
-cakes = Cake.create(
-    name: "Chocolate",
-    description: "moist chocolate cake with whipped butter cream topped with freshly picked strawberry",
-    image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Ftasty.co%2Frecipe%2Fthe-ultimate-chocolate-cake&psig=AOvVaw24a67JKZT6AXHNNX7Kn56v&ust=1686314170643000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCOj5kYHYs_8CFQAAAAAdAAAAABAD",
-    price: "$67",
-    quantity: 2
-)
+20.times do
+    Cake.create({username: Faker::Name.first_name, password: Faker::Name.last_name})
+end
 
+Creation.create({name: "Chocolate Fudge Cake", description: "Fudgey, chocolatey and definitily moisty!", image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Ftasty.co%2Frecipe%2Fthe-ultimate-chocolate-cake&psig=AOvVaw24a67JKZT6AXHNNX7Kn56v&ust=1686314170643000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCOj5kYHYs_8CFQAAAAAdAAAAABAD", price: "$68", quantity: 2})
+Creation.create({name: "Zesty Lemon Cake", description: "A zesty affair made with love for you!", image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.recipetineats.com%2Flemon-cake-with-lemon-frosting%2F&psig=AOvVaw2xqBXVSqx6hdmyBjs63euo&ust=1686419727862000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCLiqlJrhtv8CFQAAAAAdAAAAABAD", price: "25", quantity: 1 })
+Creation.create({name: "Strawberry Cheesecake", description: "She's a cheesy one!", image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fspaceshipsandlaserbeams.com%2Fstrawberry-shortcake-cheesecake%2F&psig=AOvVaw2hp5y4JKajfLfjaHUIugww&ust=1686419775322000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCKDV7bDhtv8CFQAAAAAdAAAAABAI", price: "40", quantity: 1})
+Creation.create({name: "Salted Caramel", description: "One scrumptious bite after another!", image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Frealfood.tesco.com%2Frecipes%2Fsalted-caramel-chocolate-cake.html&psig=AOvVaw0RrVFN2IT-XgSAW3nFc7By&ust=1686419828097000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCKidgNHhtv8CFQAAAAAdAAAAABAD", price: "90", quantity: 3})
+Creation.create({name: "Blueberry Cloud Cake", description: "Blueberry lining anyone?!", image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fsuncorefoods.com%2Fblogs%2Frecipes%2Fpurple-sweet-potato-buttercream-blueberry-cake&psig=AOvVaw003emHFPiBDPEWI7zuZYjt&ust=1686419885382000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCKCt_Ofhtv8CFQAAAAAdAAAAABAD", price: "70", quantity: 2})
+Creation.create({name: "Winter White Forest", description: "Knight in sweetness armour!", image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.goodhousekeeping.com%2Fuk%2Ffood%2Fg556326%2Fchristmas-cake-decorating%2F&psig=AOvVaw02idT_GgNCA2hieic8bz4S&ust=1686420069661000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCMj-nb3itv8CFQAAAAAdAAAAABAK", price: "240", quantity: 7})
+Creation.create({name: "Passion Vanilla Cake", description: "Vanilla can be passionate you know!", image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.rainbownourishments.com%2Fvegan-passion-fruit-cake%2F&psig=AOvVaw04ebM_d78tw5SJuDB4_P8t&ust=1686420153807000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCOiBtfDitv8CFQAAAAAdAAAAABAI", price: "55", quantity: 1})
+Creation.create({name: "Sparkling Kiwi Cake", description: "She's green and she sparkles!", image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DMkF3Zu6-5gg&psig=AOvVaw2hq5cOsfsN9vDfQIwKTlaz&ust=1686420469757000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCOiszfzjtv8CFQAAAAAdAAAAABAH", price: "120", quantity: }4)
+Creation.create({name: "Zucchini Muchini", description: "A vegie with the munchies!", image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.eggandplant.ch%2Frezept%2Fzucchini-cake&psig=AOvVaw1gF-5Mf77FAc7CToYwKrFB&ust=1686419671938000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCIDJ0__gtv8CFQAAAAAdAAAAABAD", price: "160", quantity: 2})
+Creation.create({name: "Fruity Goodness", description: "A fruit cake a day, keeps the blues away!", image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.swisscolony.com%2Fblog%2Fbakery%2Ffruitcake-an-enduring-holiday-tradition&psig=AOvVaw1U5Z4WJdftxVif7fg6X_gx&ust=1686420584036000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCOC9qrnktv8CFQAAAAAdAAAAABAD", price: "80", quantity: 1})
+Creation.create({name: "Vegan Carrot Cake", description: "Am I Veganese?", image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.homecookingadventure.com%2Fbest-ever-refined-sugar-free-carrot-cake%2F&psig=AOvVaw33Iuckis_9yKUJPBAF4Ui3&ust=1686420341681000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCNiYu8Tjtv8CFQAAAAAdAAAAABAQ", price: "380", quantity: 3})
+Creation.create({name: "Pinacolada & Cherry Cake", description: "Pina in a slice, colada in the bite!", image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Frecipes-for-all.com%2Fpina-colada-cake%2F&psig=AOvVaw2H81YKFmgzjW2PbPu1evY9&ust=1686420643770000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCPCDrdXktv8CFQAAAAAdAAAAABAD", price: "175", quantity: 5})
+
+
+
+# cakes = Cake.create(
+    # name: "Chocolate",
+    # description: "moist chocolate cake with whipped butter cream topped with freshly picked strawberry",
+    # image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Ftasty.co%2Frecipe%2Fthe-ultimate-chocolate-cake&psig=AOvVaw24a67JKZT6AXHNNX7Kn56v&ust=1686314170643000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCOj5kYHYs_8CFQAAAAAdAAAAABAD",
+    # price: "$67",
+    # quantity: 2
+# )
 
 
 
